0b016dfeadd069f465f269aee622892f3e5db685
Mikołaj Bogacki <35758689+Bogacki@users.noreply.github.com>
2021-07-04 23:02:08 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -3,12 +3,11 @@ import smtplib
 import datetime as dt
 import pandas
 from random import choice
+import os
 
-# 1. Update the birthdays.csv
 
-# 2. Check if today matches a birthday in the birthdays.csv
 LOGIN = "mikolaj.python@gmail.com"
-PASSWORD = "Test123!"
+PASSWORD = os.environ.get("PASSWORD")
 SERVER = "smtp.gmail.com"
 
 letters = ["letter_1.txt","letter_2.txt","letter_3.txt"]
@@ -39,12 +38,6 @@ for person in birthday_people:
                 from_addr=LOGIN,
                 msg=f"Subject: Happy Birthday! \n\n{letter_msg}")
 
-
-
-
-# 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME] with the person's actual name from birthdays.csv
-
-# 4. Send the letter generated in step 3 to that person's email address.
 
 
 
